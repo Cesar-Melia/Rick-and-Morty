163f56c8e3f193d17dafba803e368c10d102361e
Cesar Melia <cesarmeliaherraiz@gmail.com>
2021-06-02 15:26:14 +0200

    header links and title

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { CharactersPageComponent } from './pages/characters-page/characters-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { HeaderComponent } from './core/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    CharactersPageComponent,
+    ContactPageComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
